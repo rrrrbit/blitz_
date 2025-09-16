51066ba4b5a2c22d4cfbd1ea67a3fbba72e38aea
rrrrbit <jon.macagba.main@gmail.com>
2025-09-16 09:50:00 +0100

    start building mvt

---
+++ Assets/PLAYER_baseMvt.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PLAYER_baseMvt : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
